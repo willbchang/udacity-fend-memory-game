31eb4ed2ec75f518af0abe4ab62e90a48b601d8e
Will B Chang <will.b.chang@gmail.com>
2019-07-07 03:44:55 +0800

    refactor: rewrite shuffle()

--- js/app.js
+++ js/app.js
@@ -135,17 +135,12 @@ function init() {
   });
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle function from https://stackoverflow.com/a/6274381/9984029
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-  
+
   return array;
 }
