e2235f7baea2143d0b57ae6334deaf0514f7af71
Will B Chang <will.b.chang@gmail.com>
2019-10-20 10:28:58 +0800

    refactor: move function as inline function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import "./card.js"
-import "./cards.js" 
+import "./cards.js"
 
 var pairing = []; // an array to pair clicked cards
 var paired = []; // an array to store paired cards
@@ -16,7 +16,7 @@ $(function () {
 
     // avoid click matched and matching card
     if (paired.have(card) || pairing.have(card)) return;
-    
+
     if (pairing.length < 2) {
       pairing.push(card);
       card.show();
@@ -69,30 +69,31 @@ function matching(cards) {
   pairing = [];
   counter();
   cards[0].match(cards[1]) ? matched(cards) : unmatched(cards);
+
+  /**
+   * Store matched cards to paired[] and reset pairing[];
+   * the for loop is to avoid click matched cards
+   * it will check whether the pairing cards are already paired.
+   */
+  function matched(cards) {
+    paired = paired.concat(cards);
+    cards.pin();
+  }
+
+  /**
+   * Hide unmatched cards and empty pairing[]
+   * NOTICE: there is a time out
+   *  pairing[] can't be empty in event listener
+   *  otherwise card won't hide
+   * give player sometime to memorize cards' position
+   */
+  function unmatched(cards) {
+    setTimeout(() => {
+      cards.hide();
+    }, 1500);
+  }
 }
 
-/**
- * Store matched cards to paired[] and reset pairing[];
- * the for loop is to avoid click matched cards
- * it will check whether the pairing cards are already paired.
- */
-function matched(cards) {
-  paired = paired.concat(cards);
-  cards.pin();
-}
-
-/**
- * Hide unmatched cards and empty pairing[]
- * NOTICE: there is a time out
- *  pairing[] can't be empty in event listener
- *  otherwise card won't hide
- * give player sometime to memorize cards' position
- */
-function unmatched(cards) {
-  setTimeout(() => {
-    cards.hide();
-  }, 1500);
-}
 
 /**
  * Count each pairing click, update the counter
