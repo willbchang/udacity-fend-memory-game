f16226f5aa1c0d10a15f3ba07fdc1c9a2644c96a
Will B Chang <will.b.chang@gmail.com>
2019-10-21 13:25:17 +0800

    style: move functions

--- js/star.js
+++ js/star.js
@@ -25,11 +25,12 @@ function rate(count) {
   count <= 12 ? dim(3) : count <= 16 ? dim(2) : dim(1);
 }
 
+function dim(nth) {
+  $('.fa-star').eq(nth).removeClass('fa').addClass('far')
+}
+
 function reset() {
   $('.fa-star').removeClass('far').addClass('fa')
 }
 
-function dim(nth) {
-  $('.fa-star').eq(nth).removeClass('fa').addClass('far')
-}
 export { rate, reset };
