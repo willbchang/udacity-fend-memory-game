b26a4ef3be9c301b4f322497b9b5dcf70ef3fd70
Will B Chang <will.b.chang@gmail.com>
2019-10-22 20:14:53 +0800

    feature: add offClick

--- js/event.js
+++ js/event.js
@@ -2,6 +2,10 @@ export default class Event {
   static onClick(handler) {
     $('.deck').on('click', 'li', handler);
   }
+
+  static offClick() {
+    $('.deck').off('click', 'li');
+  }
   
   static onRestart(init) {
     $('.restart').click(init);
